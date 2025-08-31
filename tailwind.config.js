33ff5b321a631bd0945c592c3bea2658d64014fc
JonaLGarza <jonathanlgarza@outlook.com>
2025-08-31 02:09:38 -0600

    feat: added roboto, Pinyon Script and Viaoda Libre fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,9 +5,15 @@ module.exports = {
       "./index.html",
       "./src/**/*.{js,ts,jsx,tsx}",
     ],
-    theme: {  
-      extend: {},
+      theme: {  
+    extend: {
+      fontFamily: {
+        sans: ['Roboto', 'system-ui', 'Avenir', 'Helvetica', 'Arial', 'sans-serif'],
+        script: ['var(--font-script)', 'Pinyon Script', 'cursive'],
+        viaoda: ['Viaoda Libre', 'cursive'],
+      },
     },
+  },
     plugins: [],
   };
   
